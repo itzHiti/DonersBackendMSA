3a6b27b0d7f5d63ddf5805e010b3cefe85d1d3cd
itzHiti <karim_madi@mail.ru>
2025-11-30 23:34:00 +0500

    init commit

---
+++ src/main/java/kz/kbtu/donersbackendmsa/DonersBackendMsaApplication.java
@@ -0,0 +1,13 @@
+package kz.kbtu.donersbackendmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonersBackendMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonersBackendMsaApplication.class, args);
+    }
+
+}
